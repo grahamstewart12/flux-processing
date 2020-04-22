6a99a9f6c8c5a68d2ce78a11c9afe06c3e3a64fd
Graham Stewart <grahamstewart12@gmail.com>
2020-04-21 21:15:00 -0400

    replaced based read/write functions with faster readr equivalents, minor syntax changes in line with dplyr 1.0.0

--- combine_biomet.R
+++ combine_biomet.R
@@ -15,7 +15,6 @@
 
 # Load the required packages
 suppressWarnings(devtools::load_all("~/Desktop/RESEARCH/fluxtools"))
-library(openeddy)
 library(lubridate)
 library(tidyverse)
 
@@ -58,14 +57,10 @@ sun_time <- function(sol_ang, shoulder_n) {
   
   dplyr::case_when(
     magrittr::and(
-      sol_ang > 0,
-      before(sol_ang, ~ .x < 0, .n = shoulder_n)
-      #lag(sol_ang, 1) < 0 | lag(sol_ang, 2) < 0 | lag(sol_ang, 3) < 0
+      sol_ang > 0, before(sol_ang, ~ .x < 0, .n = shoulder_n)
     ) ~ "rise",
     magrittr::and(
-      sol_ang < 0,
-      before(sol_ang, ~ .x > 0, .n = shoulder_n)
-      #lag(sol_ang, 1) > 0 | lag(sol_ang, 2) > 0 | lag(sol_ang, 3) > 0
+      sol_ang < 0, before(sol_ang, ~ .x > 0, .n = shoulder_n)
     ) ~ "set",
     sol_ang > 0 ~ "day",
     sol_ang < 0 ~ "night"
@@ -87,8 +82,8 @@ flag_biomet_system <- function(data, vars = c(ta, lw_in, lw_out)) {
   tbl <- dplyr::select(data, !!vars)
   
   tbl %>%
-    dplyr::mutate_all(flag_repeats) %>%
-    dplyr::mutate_all(dplyr::na_if, 0) %>%
+    dplyr::mutate(dplyr::across(.fns = flag_repeats)) %>%
+    dplyr::mutate(dplyr::across(.fns = ~ dplyr::na_if(.x, 0))) %>%
     as.list() %>%
     purrr::pmap_int(~ sum(.)) %>%
     tidyr::replace_na(0L)
@@ -119,70 +114,70 @@ biomet_vars <- rlang::exprs(
   dplyr::matches(c("^ts_|^swc_|^g_"))
 )
 
-# List of vars with reps
-rep_vars <- list(
-  g = expr(list(g_1_1_1, g_2_1_1, g_3_1_1)),
-  swc = expr(list(swc_1_1_1, swc_2_1_1, swc_3_1_1)),
-  ts = expr(list(ts_1_1_1, ts_2_1_1, ts_3_1_1))
-)
-
 
 ### Load & clean up the Biomet file ============================================
 
 # Read in Biomet file
-biomet <- read.csv(biomet_input, stringsAsFactors = FALSE)
+biomet <- readr::read_csv(
+  biomet_input, guess_max = 6000, 
+  col_types = readr::cols(.default = readr::col_guess())
+)
 
 # Parse POSIX timestamp
-biomet <- mutate(biomet, timestamp = ymd_hms(timestamp, tz = md$tz_name))
+#biomet <- dplyr::mutate(
+#  biomet, timestamp = lubridate::ymd_hms(timestamp, tz = md$tz_name)
+#)
 
 # Simplify non-replicated variable names (remove "_1_1_1")
 biomet <- biomet %>%
-  as_tibble() %>%
-  rename_at(
-    vars(matches("_\\d_1_1"), -matches(c("^ts_|^swc_|^g_"))), 
-    str_replace, "_\\d_1_1", ""
+  dplyr::rename_with(
+    ~ stringr::str_replace(., "_\\d_1_1", ""),
+    c(dplyr::matches("_\\d_1_1"), -dplyr::matches(c("^ts_|^swc_|^g_"))),
   )
 
 # Flag/clean system errors indicated by simultaneous runs in biomet vars
 biomet <- biomet %>% 
-  mutate(qc_biomet_all = flag_biomet_system(.)) %>%
-  mutate_at(vars(!!!biomet_vars), ~ clean(., qc_biomet_all))
+  dplyr::mutate(qc_biomet_all = flag_biomet_system(.)) %>%
+  dplyr::mutate(dplyr::across(c(!!!biomet_vars), ~ clean(.x, qc_biomet_all)))
 
 
 ### Correct units ==============================================================
 
 # Check p_rain before converting
 if (max(biomet$p_rain, na.rm = TRUE) < 1) {
-  biomet <- mutate(biomet, p_rain = p_rain * 1000)
+  biomet <- dplyr::mutate(biomet, p_rain = p_rain * 1000)
 }
 
 # Automatic correction of temperatures and percentages
 biomet <- biomet %>%
-  mutate_at(vars(starts_with("swc_")), ~ if_else(. > 1, . / 100, .)) %>%
-  mutate_at(
-    vars(starts_with("ts_"), ta, ta_ep), ~ if_else(. > 150, . - 273.15, .)
-  )
+  dplyr::mutate(dplyr::across(
+    dplyr::starts_with("swc_"), ~ dplyr::if_else(.x > 1, .x / 100, .x)
+  )) %>%
+  dplyr::mutate(dplyr::across(
+    c(dplyr::starts_with("ts_"), ta, ta_ep), 
+    ~ dplyr::if_else(.x > 150, .x - 273.15, .x)
+  ))
 
 # CH4 variables from nmol to umol
 if (median(biomet$fch4, na.rm = TRUE) > 1) {
-  biomet <- mutate_at(
-    biomet, vars(
+  biomet <- dplyr::mutate(biomet, dplyr::across(
+    c(
       fch4, fch4_randunc_hf, sch4_single, ch4_molar_density, ch4_mixing_ratio, 
-      ch4, ch4_meas_median, ch4_meas_p25, ch4_meas_p75, ch4_meas_sigma
-    ), ~ . / 1000
-  )
+      ch4, ch4_meas_sigma
+    ), ~ .x / 1000
+  ))
 }
 
 
 ### Correct wind direction for magnetic declination ============================
 
 # Calculate magnetic declination given location & time
-mag_decl <- pluck(
+mag_decl <- purrr::pluck(
   oce::magneticField(md$lon, md$lat, settings$year + 0.5), "declination"
 )
 
 # Reassign magnetic wind direction, apply declination
-biomet <- mutate(
+biomet <- dplyr::mutate(
   biomet,
   wd_mag = wd,
   wd = (wd_mag - mag_decl) %% 360
@@ -191,12 +186,14 @@ biomet <- mutate(
 
 ### Combined replicated variables ==============================================
 
-biomet <- mutate(
-  biomet,
-  g = pmap_dbl(!!rep_vars$g, lift_vd(mean, na.rm = TRUE)),
-  swc = pmap_dbl(!!rep_vars$swc, lift_vd(mean, na.rm = TRUE)),
-  ts = pmap_dbl(!!rep_vars$ts, lift_vd(mean, na.rm = TRUE))
-)
+biomet <- biomet %>%
+  dplyr::rowwise() %>%
+  dplyr::mutate(
+    g = mean(c(g_1_1_1, g_2_1_1, g_3_1_1), na.rm = TRUE),
+    swc = mean(c(swc_1_1_1, swc_2_1_1, swc_3_1_1), na.rm = TRUE),
+    ts = mean(c(ts_1_1_1, ts_2_1_1, ts_3_1_1), na.rm = TRUE)
+  ) %>%
+  dplyr::ungroup()
 
 
 ### Correct radiation values for zero-offset ===================================
@@ -204,20 +201,22 @@ biomet <- mutate(
 # Radiation offset
 offset <- biomet %>%
   # Calculate sw_in_pot and sol_ang at end of averaging period
-  mutate(
+  dplyr::mutate(
     sw_in_pot = potential_radiation(timestamp, md),
     sol_ang = sun_position(timestamp, md),
     # Stricter sun time to exclude any possible period with light
     sun_time = sun_time(sol_ang, shoulder_n = 3)
   ) %>%
-  filter(sw_in_pot == 0, sun_time == "night") %>%
-  summarize_at(vars(ppfd_in, sw_in, sw_out), mean, na.rm = TRUE) %>%
+  dplyr::filter(sw_in_pot == 0, sun_time == "night") %>%
+  dplyr::summarize(dplyr::across(
+    c(ppfd_in, sw_in, sw_out), mean, na.rm = TRUE)
+  ) %>%
   as.list()
 
 # Check offsets
 #offset %>% enframe(value = "offset") %>% unnest(offset)
 
-biomet <- mutate(
+biomet <- dplyr::mutate(
   biomet, 
   ppfd_in = apply_offset(ppfd_in, -offset$ppfd_in),
   sw_in = apply_offset(sw_in, -offset$sw_in),
@@ -231,13 +230,13 @@ biomet <- mutate(
 
 # Get coefficient of ppfd_in/sw_in relationship
 frac_ppfd <- biomet %>% 
-  select(sw_in, ppfd_in) %>% 
-  drop_na() %>% 
-  summarize(sum(sw_in) / sum(ppfd_in)) %>% 
-  pluck(1)
+  dplyr::select(sw_in, ppfd_in) %>% 
+  tidyr::drop_na() %>% 
+  dplyr::summarize(sum(sw_in) / sum(ppfd_in)) %>% 
+  purrr::pluck(1)
 #frac_ppfd
 
-biomet <- mutate(
+biomet <- dplyr::mutate(
   biomet,
   # Calculate sw_in equivalent of ppfd_in
   sw_in_ppfd = ppfd_in * frac_ppfd,
@@ -267,7 +266,7 @@ biomet_docu <- append(
     offsets = offset
   )
 )
-biomet_docu_out <- str_replace(biomet_out, ".csv", ".txt")
+biomet_docu_out <- stringr::str_replace(biomet_out, ".csv", ".txt")
 
 # Save documentation
 sink(biomet_docu_out)
