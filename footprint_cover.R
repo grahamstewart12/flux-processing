6d2b8dad312e43e300bc1598be7e2fb18faabe01
Graham Stewart <grahamstewart12@gmail.com>
2020-04-23 13:30:18 -0400

    tweaked methods for more efficient matrix convolution, performance enhancements to just about every step

--- footprint_cover.R
+++ footprint_cover.R
@@ -27,17 +27,6 @@ enlist_names <- function(x) {
     purrr::map(~ NULL)
 }
 
-# Performs a vectorized function on x and returns x as a matrix
-with_matrix <- function(x, .f) {
-  
-  dims <- dims(x)
-  mat_fun <- rlang::as_function(.f)
-  
-  vec <- mat_fun(as.vector(x))
-  
-  matrix(vec, nrow = dims[1], ncol = dims[2])
-}
-
 progress_info <- function(len) {
   
   progress_bar$new(
@@ -116,11 +105,17 @@ fp_path <- latest_version(path_in, "half_hourly_ffp", "")
 # Set tag for creating output file names
 tag_out <- create_tag(settings$site, settings$year, settings$date)
 
+# Set path for output files
+path_out <- file.path(wd, "processing_data", "13_footprint_cover", "output")
+
 
 ### Load required input data ===================================================
 
 # Import water level data
-wtd <- readr::read_csv(wtd_path)
+site_wtd <- readr::read_csv(
+  wtd_path, guess_max = 6000, 
+  col_types = readr::cols(.default = readr::col_guess())
+)
 
 # Import footprint grid (including AOI grid)
 grid <- read_grid(file.path(fp_path, "grid"), names = c("x", "y", "aoi"))
@@ -128,19 +123,13 @@ grid <- read_grid(file.path(fp_path, "grid"), names = c("x", "y", "aoi"))
 # Import site image
 img <- raster::raster(img_path)
 #img_key <- c("wtr" = 1, "veg" = 2, "frs" = 3)
-#img_key <- tibble::tibble(name = c("wtr", "veg", "frs"), value = c(1, 2, 3))
-#plot_matrix(img_grid)
-#img <- raster::setValues(img, factor(raster::values(img)))
 
 # Import DEM
 dem <- raster::raster(dem_path)
-#plot_matrix(dem_grid)
 
 demw <- raster::raster(demw_path)
 #flood_key <- c("dry" = -1, "wet" = 1)
-#flood_key <- tibble::tibble(name = c("wet", "dry"), value = c(1, -1))
-#plot_matrix(demw_grid)
-browser()
+
 
 ### Prepare input data =======================================
 
@@ -154,19 +143,16 @@ dem_grid <- snap_to_grid(dem, grid, c(md$x_utm, md$y_utm))
 demw_grid <- snap_to_grid(demw, grid, c(md$x_utm, md$y_utm))
 
 # Reclassify cover types with labels
-img_grid <- with_matrix(
-  img_grid, ~ dplyr::recode(.x, `1` = "wtr", `2` = "veg", `3` = "frs")
-)
+#img_grid <- with_matrix(
+#  img_grid, ~ dplyr::recode(.x, `1` = "wtr", `2` = "veg", `3` = "frs")
+#)
 
 # Create key for combined cover types & flooding
-#key <- img_key %>% 
-#  purrr::map(~ .x * flood_key) %>%
-#  purrr::flatten_dbl() %>%
-#  rlang::set_names(
-#    names(img_key) %>% 
-#      purrr::map(~ stringr::str_c(.x, names(flood_key), sep = "_")) %>%
-#      purrr::flatten_chr()
-#  )
+key <- img_key %>% 
+  purrr::map(~ .x * flood_key) %>% 
+  purrr::imap(~ rlang::set_names(.x, stringr::str_c(.y, "_", names(.)))) %>%
+  purrr::flatten() %>%
+  purrr::simplify()
 
 
 ### Retrieve footprints, calculate cover =======================================
@@ -181,49 +167,57 @@ fp_files <- list.files(file.path(fp_path, "footprints"), full.names = TRUE)
 fp_timestamps <- fp_files %>%
   basename() %>%
   stringr::str_sub(1, -5) %>%
-  lubridate::ymd_hms(tz = md$tz_name)
+  lubridate::ymd_hms()
 cover <- vector("list", length = length(fp_files))
-wtd <- site %>%
-  dplyr::filter(timestamp %in% fp_timestamps) %>%
+wtd <- site_wtd %>%
+  dplyr::right_join(
+    tibble::enframe(fp_timestamps, name = NULL, value = "timestamp")
+  ) %>%
   dplyr::pull(wtd_f)
 p <- progress_info(length(fp_files))
 
 for (i in 1:length(fp_files)) {
-  
+  browser()
   # Read and scale footprint matrix
-  fp_temp <- read_matrix(fp_files[i])
-  
+  fp_temp <- read_matrix(fp_files[i]) # 6.61 ms
+
   # Mask footprint to 85% to account for rapid expansion upon approaching 100%
   # - recommended: between 80% and 90% (Kljun et al. 2015)
   # - this means data should be first filtered for phi > 0.85
-  fp_mask <- mask_source_area(fp_temp, p = 0.85)
-  #fp_temp <- fp_percent(fp_temp, 0.85, fill = 0)
+  fp_mask <- mask_source_area(fp_temp, p = 0.85, mask_value = 0) # 4.17 ms
+  fp_mask <- fp_mask * grid$aoi # 0.192 ms
   
   # Normalize (???)
   #fp_temp <- raster::calc(fp_temp, function(x) x / sum(x, na.rm = TRUE))
   
   # Calculate water level over AOI
-  aoi_wtd <- md$elev_well + wtd[i] - demw_re
+  #aoi_wtd <- md$elev_tower + wtd[i] - demw_grid # 0.131 ms
+  # Give wtd a placeholder value if it is NA, so that other covers can be summed
+  aoi_wtd <- md$elev_tower + tidyr::replace_na(wtd[i], 0.1) - demw_grid
   # Classify flooded area
-  aoi_flooded <- ifelse(aoi_wtd >= 0, "wet", "dry")
+  aoi_flooded <- with_matrix(aoi_wtd, ~ dplyr::if_else(.x >= 0, 1, 0)) 
+  # 3.15 ms
+  #aoi_flooded <- ifelse(aoi_wtd >= 0, "wet", "dry") # 7.80 ms
   
   # Can use cross_grids() if want to combine more than 2 matrices
   # - this is kind of awkward but flexible
   # - automatically detects if one matrix is all NA and drops it
   # - ensures that all cells are classified properly
-  aoi_cover <- cross_grids(img_grid, aoi_flooded)
+  #aoi_cover <- cross_grids(img_grid, aoi_flooded) # 5.52 ms
+  aoi_cover <- img_grid * sign(aoi_wtd)
   
   # Calculate cover-type weights
-  cover_wt <- summarize_cover(fp_temp, aoi_cover)
+  cover_wt <- summarize_cover(fp_mask, aoi_cover, levels = key) # 3.79 ms
+  # CHECK: sum of these should equal sum(fp_temp)
   
-  # Calculate footprint-weighted WTD
-  wtd_wt <- summarize_cover(fp_temp, aoi_wtd, type = "numeric")
+  # Calculate footprint-weighted WTD & flooded area
+  wtd_wt <- summarize_cover(fp_mask, aoi_wtd, type = "numeric") # 0.605 ms
+  flood_wt <- summarize_cover(fp_mask, aoi_flooded, type = "numeric") # 0.605 ms
   
   # Add all weights to list 
-  cover[[i]] <- c(cover_wt, "wtd_wt" = wtd_wt)
-  
+  cover[[i]] <- c(cover_wt, "wtd_wt" = wtd_wt, "flood_wt" = flood_wt)
+
   p$tick()
-  
 }
 
 # Gather everything into one data frame
@@ -243,19 +237,8 @@ fp_cover <- fp_cover %>%
     dry = wtr_dry + veg_dry + frs_dry
   )
 
-# Check patch coverage distributions
-covers %>%
-  select(-wtd, -wtd_wt, -wtr_dry, -veg_dry) %>%
-  gather("type", "wt", -timestamp) %>%
-  ggplot(aes(wt, fill = type, color = type)) +
-  #facet_wrap(~ type, scales = "free") +
-  geom_density(alpha = 0.2)
-
 # Write output to file
-covers_out <- file.path(
-  wd, "processing_data", "13_footprint_cover", "output", 
-  paste0("footprint_cover_", tag_out, ".csv")
-)
-write_eddy(all_covers, covers_out)
+covers_out <- file.path(path_out, paste0("footprint_cover_", tag_out, ".csv"))
+readr::write_csv(all_covers, covers_out)
 
 
