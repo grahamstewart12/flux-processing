26bb2b091d42b1fb5326a3f2544ebde4cfd02d6a
Graham Stewart <grahamstewart12@gmail.com>
2020-04-24 20:45:45 -0400

    add support for more than one latest versions

--- functions/latest_version.R
+++ functions/latest_version.R
@@ -2,27 +2,31 @@
 # Find the latest version of an output file
 latest_version <- function(dir, name, ext = ".csv", n = 1) {
   
-  # Add path separator to dir (if needed)
-  if (stringr::str_sub(dir, -1) != "/") dir <- stringr::str_c(dir, "/")
-  # Add string separator to name (if needed)
-  if (stringr::str_sub(name, -1) != "_") name <- stringr::str_c(name, "_")
+  # Get files matching extension
+  files <- dir %>% list.files() %>% stringr::str_subset(ext)
   
-  # Get files matching name and extension
-  files <- list.files(dir) %>%
-    stringr::str_subset(paste0("\\b", name, "[:upper:]")) %>%
-    stringr::str_subset(ext)
+  # If name is missing, assume all files in dir have the same name
+  if (!missing(name)) {
+    
+    # Add string separator to name (if needed)
+    if (stringr::str_sub(name, -1) != "_") name <- stringr::str_c(name, "_")
+    
+    # Get files matching name
+    files <- stringr::str_subset(files, paste0("\\b", name, "[:upper:]"))
+  }
   
-  # Find file with the latest date tag
+  # Find file(s) with the latest date tag
   ext_len <- stringr::str_length(ext)
+  
   dates <- files %>%
     stringr::str_sub(-10 - ext_len, -1 - ext_len) %>%
-    lubridate::ymd()
-  date <- dates[which(dates == dplyr::last(sort(dates)))]
-  out <- stringr::str_subset(files, as.character(date))
+    lubridate::ymd() %>%
+    vctrs::vec_sort(direction = "desc") %>%
+    vctrs::vec_chop(list(1:n)) %>%
+    purrr::pluck(1)
   
-  # Choose longest file name if multiple files still remain
-  # TODO (maybe) allow n > 1
-  if (length(out) > 1) out <- dplyr::first(out, stringr::str_length(out))
-  paste0(dir, out)
-  
+  out <- stringr::str_subset(files, stringr::str_c(dates, collapse = "|"))
+
+  # Return path to latest file(s)
+  file.path(dir, out)
 }
