11ace47d6733197969cf0d1f060ccf98dc23bacf
Graham Stewart <grahamstewart12@gmail.com>
2020-06-10 21:17:32 -0400

    new dots argument allows multiple flags to be passed which are then combined with help from 'combine_flags()'

--- functions/clean.R
+++ functions/clean.R
@@ -1,5 +1,17 @@
 
-clean <- function(x, flag, value = 2, replace_with = NA_real_, na.as = 0) {
+# Depends on functions/flag.R
+source("~/Desktop/DATA/Flux/tools/engine/functions/flag.R")
+
+clean <- function(x, ..., value = 2, replace_with = NA_real_, na.as = 0) {
+  
+  dots <- rlang::list2(...)
+  
+  # If multiple flags were passed, combine them
+  if (length(dots) > 1) {
+    flag <- combine_flags(!!!dots)
+  } else {
+    flag <- purrr::pluck(dots, 1)
+  }
   
   if (is.character(value)) {
     flag <- dplyr::if_else(flag == value, 2, 0)
