ecbf2a1fcea21b252f5bc0ec36eef7e8ac1035a8
Graham Stewart <grahamstewart12@gmail.com>
2020-06-10 21:06:31 -0400

    add experimental 'complete_time_seq()'

--- functions/dates_and_times.R
+++ functions/dates_and_times.R
@@ -39,7 +39,7 @@ remove_time_comps <- function(data, ...) {
 }
 
 # Generate Year-long Half-hourly Time Step Vector
-create_timesteps <- function(year, dts = 48, tz = "GMT", shift_by = 720 / dts) {
+create_timesteps <- function(year, dts = 48, tz = "UTC", shift_by = 720 / dts) {
   
   year <- as.numeric(year)
   
@@ -56,4 +56,49 @@ create_timesteps <- function(year, dts = 48, tz = "GMT", shift_by = 720 / dts) {
     strptime(start, format, tz), strptime(end, format, tz), (24 / dts * 60 * 60)
   )
   out
+}
+
+complete_time_seq <- function(data, time, time_diff, .first, .last) {
+  
+  # Not using yet--risky if timestamp doesn't start/end on start/end of year
+  
+  # Determine time var if not given
+  if (missing(time)) {
+    
+    time_name <- data %>%
+      purrr::map(is.POSIXct) %>%
+      purrr::keep(~ .) %>%
+      names() %>%
+      vctrs::vec_slice(1)
+    
+    time <- rlang::sym(time_name)
+  } else {
+    
+    time_name <- rlang::ensym(time) %>% rlang::as_string()
+    time <- rlang::enquo(time)
+  }
+  
+  time_seq <- dplyr::pull(data, !!time)
+  if (missing(.first)) .first <- dplyr::first(time_seq)
+  if (missing(.last)) .last <- dplyr::last(time_seq)
+  
+  # Determine interval if not given
+  # TODO this should be its own function 'get_time_diff'
+  if (missing(time_diff)) {
+    time_diff <- time_seq %>%
+      lubridate::int_diff() %>%
+      lubridate::as.duration() %>%
+      vctrs::vec_count() %>%
+      vctrs::vec_slice(1) %>%
+      purrr::pluck("key", 1)
+  } else {
+    time_diff <- lubridate::duration(time_diff)
+  }
+  
+  # Create sequence vector
+  full_seq <- tibble::tibble(
+    !!time_name := seq(.first, .last, by = time_diff)
+  )
+  
+  dplyr::left_join(full_seq, data, by = time_name)
 }
