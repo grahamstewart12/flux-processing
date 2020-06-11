2463128363317b206e55d7404b8c1052ac8f1f20
Graham Stewart <grahamstewart12@gmail.com>
2020-06-10 21:05:18 -0400

    this will probably get moved to another repository, but lots of edits to improve flow and play nicer with matrices

--- processing/07_footprint_cover.R
+++ processing/07_footprint_cover.R
@@ -8,14 +8,22 @@
 
 # Output(s):
 
+control <- list(
+  phi = 0.85
+)
+
 # Load the required packages
-suppressWarnings(devtools::load_all("~/Desktop/RESEARCH/fluxtools"))
-devtools::load_all("~/R Projects/footprints")
+devtools::load_all("/Users/Graham/R Projects/footprints")
 library(progress)
 library(lubridate)
 library(tidyverse)
 
-source("~/Desktop/DATA/Flux/tools/reference/site_metadata.R")
+# Load reference files
+source("/Users/Graham/Desktop/DATA/Flux/tools/reference/site_metadata.R")
+
+# Load functions
+source("~/Desktop/DATA/Flux/tools/engine/functions/latest_version.R")
+source("~/Desktop/DATA/Flux/tools/engine/functions/utilities.R")
 
 
 ### Helper functions ===========================================================
@@ -70,77 +78,79 @@ md <- purrr::pluck(site_metadata, settings$site)
 
 # Set the desired working directory in RStudio interface
 # - assumes that the subdirectory structure is already present
-wd <- file.path("~/Desktop", "DATA", "Flux", settings$site, settings$year)
-path_in <- file.path(wd, "processing_data", "06_footprint", "output")
+wd <- file.path(
+  "/Users/Graham/Desktop", "DATA", "Flux", settings$site, settings$year
+)
+path_in <- file.path(wd, "processing", "05_footprint")
 
 # Set the file names
 # Half-hourly water level
-wtd_path <- file.path(
-  "~/Desktop", "DATA", "PT", "output", settings$site, 
+path_wtd <- file.path(
+  "/Users/Graham/Desktop", "DATA", "PT", "output", settings$site, 
   paste0(settings$site, "_", settings$year, ".csv")
 )
 
-# Cover type image
-img_path <- file.path(
-  dirname(wd), "imagery", "image_classification", "output", "classified.tif"
+# Cover type raster
+path_class <- file.path(
+  dirname(wd), "analysis", "spatial", "07_classification", "classes_ens.tif"
 )
 
-# DEM
-dem_path <- file.path(
-  dirname(wd), "raster_processing", paste0(settings$site, "_DEM_proc.tif")
-)
-
-# WTD-corrected DEM
-demw_path <- file.path(
-  dirname(wd), "raster_processing", paste0(settings$site, "_DEM_wtdcorr.tif")
+# Relative elevation raster
+path_elev <- file.path(
+  dirname(wd), "analysis", "spatial", "08_relative_elevation", "rel_elev.tif"
 )
 
 # TODO import other imagery into a big list for footprint weights 
 # - e.g. NDVI, NDWI, texture stuff
 
-aoi_path <- file.path(dirname(wd), paste0(settings$site, "_area_proc"))
+path_delin <- file.path(dirname(wd), "site_info", "delineation")
 
-fp_path <- latest_version(path_in, "half_hourly_ffp", "")
+path_fp <- latest_version(file.path(path_in, "footprint"), "halfhourly_K15", "")
+path_phi <- latest_version(file.path(path_in, "stats"), "footprint_stats")
 
 # Set tag for creating output file names
 tag_out <- create_tag(settings$site, settings$year, settings$date)
 
 # Set path for output files
-path_out <- file.path(wd, "processing_data", "13_footprint_cover", "output")
+path_out <- file.path(wd, "processing", "07_footprint_cover", "data")
 
 
 ### Load required input data ===================================================
 
 # Import water level data
-site_wtd <- readr::read_csv(
-  wtd_path, guess_max = 6000, 
+data_wtd <- readr::read_csv(
+  path_wtd, guess_max = 6000, 
+  col_types = readr::cols(.default = readr::col_guess())
+)
+
+# Import footprint phi data
+data_phi <- readr::read_csv(
+  path_phi, guess_max = 6000, 
   col_types = readr::cols(.default = readr::col_guess())
 )
 
 # Import footprint grid (including AOI grid)
-grid <- read_grid(file.path(fp_path, "grid"), names = c("x", "y", "aoi"))
+grid <- read_grid(file.path(path_fp, "grid"), names = c("x", "y", "aoi"))
 
-# Import site image
-img <- raster::raster(img_path)
-#img_key <- c("wtr" = 1, "veg" = 2, "frs" = 3)
+# Import classified image
+class <- raster::raster(path_class)
+class_key <- c("wtr" = 1, "veg" = 2, "frs" = 3)
 
-# Import DEM
-dem <- raster::raster(dem_path)
-
-demw <- raster::raster(demw_path)
-#flood_key <- c("dry" = -1, "wet" = 1)
+# Import relative elevation
+rel_elev <- raster::raster(path_elev)
+flood_key <- c("dry" = -1, "wet" = 1)
 
 
-### Prepare input data =======================================
+### Prepare input data =========================================================
 
 # Get measurement elevation (from NON-CORRECTED DEM)
 #meas_elev <- raster::extract(dem, data.frame(x = md$x_utm, y = md$y_utm))
 
 # Map imagery onto same grid as footprint
-img_grid <- snap_to_grid(img, grid, c(md$x_utm, md$y_utm))
+class_grid <- snap_to_grid(class, grid, c(md$x_utm, md$y_utm))
 # use bilinear interpolation for DEM?
-dem_grid <- snap_to_grid(dem, grid, c(md$x_utm, md$y_utm))
-demw_grid <- snap_to_grid(demw, grid, c(md$x_utm, md$y_utm))
+elev_grid <- snap_to_grid(rel_elev, grid, c(md$x_utm, md$y_utm))
+#demw_grid <- snap_to_grid(demw, grid, c(md$x_utm, md$y_utm))
 
 # Reclassify cover types with labels
 #img_grid <- with_matrix(
@@ -148,7 +158,7 @@ demw_grid <- snap_to_grid(demw, grid, c(md$x_utm, md$y_utm))
 #)
 
 # Create key for combined cover types & flooding
-key <- img_key %>% 
+key <- class_key %>% 
   purrr::map(~ .x * flood_key) %>% 
   purrr::imap(~ rlang::set_names(.x, stringr::str_c(.y, "_", names(.)))) %>%
   purrr::flatten() %>%
@@ -158,44 +168,75 @@ key <- img_key %>%
 ### Retrieve footprints, calculate cover =======================================
 
 # Get list of footprint files
-fp_files <- list.files(file.path(fp_path, "footprints"), full.names = TRUE)
+fp_files <- list.files(file.path(path_fp, "footprints"), full.names = TRUE)
 
 # Initialize loop
 # should I read all matrices first and then perform calculations??
 
 # Empty list with names from footprint files (i.e. the timestamps)
+
 fp_timestamps <- fp_files %>%
   basename() %>%
   stringr::str_sub(1, -5) %>%
-  lubridate::ymd_hms()
-cover <- vector("list", length = length(fp_files))
-wtd <- site_wtd %>%
+  lubridate::ymd_hms() %>%
+  tibble::enframe(name = NULL, value = "timestamp") %>%
+  dplyr::left_join(
+    dplyr::select(data_phi, timestamp, phi = 2), by = "timestamp"
+  ) %>%
+  dplyr::filter(phi >= control$phi)
+
+fp_files <- path_fp %>% 
+  file.path("footprints") %>%
+  list.files(full.names = TRUE) %>%
+  tibble::enframe(name = NULL, value = "file") %>%
+  dplyr::mutate(
+    timestamp = file %>% 
+      basename() %>% 
+      stringr::str_sub(1, -5) %>% 
+      lubridate::ymd_hms(),
+    .before = 1
+  ) %>%
+  dplyr::left_join(
+    dplyr::select(data_phi, timestamp, phi = 2), by = "timestamp"
+  ) %>%
+  dplyr::filter(phi >= control$phi) %>%
+  dplyr::select(-phi) %>%
+  tibble::deframe()
+
+fp_timestamps <- fp_files %>% names() %>% lubridate::ymd_hms()
+
+n_fp <- length(fp_files)
+cover <- vctrs::vec_init(list(), n_fp)
+wtd <- data_wtd %>%
   dplyr::right_join(
-    tibble::enframe(fp_timestamps, name = NULL, value = "timestamp")
+    tibble::enframe(fp_timestamps, name = NULL, value = "timestamp"),
+    by = "timestamp"
   ) %>%
   dplyr::pull(wtd_f)
-p <- progress_info(length(fp_files))
+p <- progress_info(n_fp)
 
-for (i in 1:length(fp_files)) {
-  browser()
+for (i in 1:n_fp) {
+  #browser()
   # Read and scale footprint matrix
   fp_temp <- read_matrix(fp_files[i]) # 6.61 ms
 
   # Mask footprint to 85% to account for rapid expansion upon approaching 100%
   # - recommended: between 80% and 90% (Kljun et al. 2015)
   # - this means data should be first filtered for phi > 0.85
-  fp_mask <- mask_source_area(fp_temp, p = 0.85, mask_value = 0) # 4.17 ms
+  fp_mask <- mask_source_area(fp_temp, p = control$phi, mask_value = 0) # 4.17 ms
   fp_mask <- fp_mask * grid$aoi # 0.192 ms
   
-  # Normalize (???)
+  # Normalize (???) - I think not necessary, summarize_cover does it 
   #fp_temp <- raster::calc(fp_temp, function(x) x / sum(x, na.rm = TRUE))
   
   # Calculate water level over AOI
-  #aoi_wtd <- md$elev_tower + wtd[i] - demw_grid # 0.131 ms
+  # - as depth relative to land surface (negative = water below surface)
+  #aoi_wtd <- md$rel_elev_well + wtd[i] - elev_grid # 0.131 ms
   # Give wtd a placeholder value if it is NA, so that other covers can be summed
-  aoi_wtd <- md$elev_tower + tidyr::replace_na(wtd[i], 0.1) - demw_grid
-  # Classify flooded area
+  aoi_wtd <- md$rel_elev_well + tidyr::replace_na(wtd[i], 0.1) - elev_grid
+  # Classify flooded area (1 = wet, -1 = dry)
   aoi_flooded <- with_matrix(aoi_wtd, ~ dplyr::if_else(.x >= 0, 1, 0)) 
+  # aoi_flooded <- sign(aoi_wtd)
   # 3.15 ms
   #aoi_flooded <- ifelse(aoi_wtd >= 0, "wet", "dry") # 7.80 ms
   
@@ -204,7 +245,7 @@ for (i in 1:length(fp_files)) {
   # - automatically detects if one matrix is all NA and drops it
   # - ensures that all cells are classified properly
   #aoi_cover <- cross_grids(img_grid, aoi_flooded) # 5.52 ms
-  aoi_cover <- img_grid * sign(aoi_wtd)
+  aoi_cover <- class_grid * aoi_flooded
   
   # Calculate cover-type weights
   cover_wt <- summarize_cover(fp_mask, aoi_cover, levels = key) # 3.79 ms
