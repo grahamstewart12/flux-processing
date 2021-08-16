1d830c2542613a3cc3065bd948ac8dad50c4476a
Graham Stewart <grahamstewart12@gmail.com>
2021-08-16 12:08:47 -0400

    So many changes, way too long since last commit. Everything towards streamlining and dismantling routines that became overly complex.

--- processing/01_combine_eddypro.R
+++ processing/01_combine_eddypro.R
@@ -13,22 +13,13 @@
 start_time <- Sys.time()
 
 # Load the required packages
-#suppressWarnings(devtools::load_all("~/Desktop/RESEARCH/fluxtools"))
-library(openeddy, warn.conflicts = FALSE)
+devtools::load_all("~/Projects/flux/dscalr")
 library(lubridate, warn.conflicts = FALSE)
 library(tidyverse, warn.conflicts = FALSE)
 
 # Load reference files
-path_ref <- "/Users/Graham/Desktop/DATA/Flux/tools/reference"
-source(file.path(path_ref, "combine_eddypro_control.R"))
-source(file.path(path_ref, "site_metadata.R"))
-
-# Load functions
-path_funs <- "/Users/Graham/Desktop/DATA/Flux/tools/engine/functions"
-source(file.path(path_funs, "attributes.R"))
-source(file.path(path_funs, "dates_and_times.R"))
-source(file.path(path_funs, "read_eddypro.R"))
-source(file.path(path_funs, "utilities.R"))
+# source("reference/combine_eddypro_control.R")
+# source("reference/site_metadata.R")
 
 
 ### Helper functions ===========================================================
@@ -39,6 +30,8 @@ str_subset_eddypro <- function(files) {
   keep <- names %>% 
     stringr::str_subset("^eddypro_") %>%
     stringr::str_subset("\\.csv$") %>%
+    # Remove spectral analysis files
+    stringr::str_subset("spectra", negate = TRUE) %>%
     # Risky subset to remove "user stats" files from consideration
     stringr::str_subset("user", negate = TRUE)
   inds <- which(names %in% keep)
@@ -48,125 +41,85 @@ str_subset_eddypro <- function(files) {
 
 ### Initialize script settings & documentation =================================
 
+# Load metadata file
+md <- yaml::read_yaml(file.path("data", settings$site, "metadata.yml"))
+
 # Set directory where outputs are stored
 # This assumes that the subdirectory structure is already present
-wd <- file.path(
-  "/Users/Graham/Desktop", "DATA", "Flux", settings$site, settings$year
-)
+wd <- file.path("data", settings$site, settings$year)
 
 # Set the folders containing the EddyPro outputs to be combined
-ep_dirs <- purrr::pluck(
-  control, settings$site, as.character(settings$year), "ep_dirs"
+# ep_dirs <- pluck(
+#   control, settings$site, as.character(settings$year), "ep_dirs"
+# )
+ep_paths <- list.dirs(
+  file.path(wd, "00_raw_output", "eddypro"), recursive = FALSE
 )
-ep_paths <- file.path(wd, "processing", "00_raw_output", "eddypro", ep_dirs)
-
-# Load metadata file
-md <- purrr::pluck(site_metadata, settings$site)
+#ep_paths <- file.path(wd, "00_raw_output", "eddypro", ep_dirs)
+ep_dirs <- basename(ep_paths)
 
 # Set tag for creating output file names
-tag_out <- create_tag(settings$site, settings$year, settings$date)
+tag <- make_tag(settings$site, settings$year)
 
 # Set path for output files
-path_out <- file.path(wd, "processing", "01_combine_eddypro")
+path_out <- file.path(wd, "01_combine_eddypro")
 
 
 ### Load, combine, and save the EddyPro files ==================================
 
+# Read EddyPro settings
+ep_settings <- ep_paths %>%
+  map(list.files, pattern = ".eddypro", full.names = TRUE) %>%
+  map(read_eddypro_settings) %>%
+  map(as_tibble) %>%
+  map2(ep_dirs, ~ mutate(.x, dir = .y)) %>%
+  map(relocate, dir)
+# all.equal(ep_settings[[1]], ep_settings[[2]])
+
+# Find all EddyPro data files
+data_files <- ep_paths %>% 
+  map(list.files, pattern = ".csv", full.names = TRUE, recursive = TRUE) %>% 
+  map(str_subset_eddypro)
+file_types <- map(data_files, get_eddypro_file_type)
+
+# Read EddyPro data (files may be large--be patient)
+# data_ep <- data_files %>% 
+#   map2(file_types, ~ map2(.x, .y, ~ read_eddypro(.x, type = .y))) %>% 
+#   map(rlang::set_names, file_types)
+
 # Set the lists to hold different types of output files
-ep_settings <- vctrs::vec_init(list(), length(ep_paths))
-
-lists <- 1:6 %>%
-  purrr::map(~ vctrs::vec_init(list(), length(ep_paths))) %>%
-  rlang::set_names(
-    c("full_output", "metadata", "biomet", "fluxnet", "qc_details", "st7")
-  )
-
-data_files <- vctrs::vec_init(list(), length(ep_paths))
+data_ep <- ep_dirs %>% 
+  map2(data_files, ~ vctrs::vec_init(list(), length(.y))) %>%
+  rlang::set_names(ep_dirs) %>%
+  map2(file_types, ~ rlang::set_names(.x, .y))
 
 # Read files into the lists
-for (i in seq_along(ep_paths)) {
+for (i in seq_along(ep_dirs)) {
   
   cat("Reading files from folder ", basename(ep_paths[i]), ": ", sep = "")
   
-  # Read EddyPro settings
-  ep_settings[[i]] <- ep_paths[i] %>%
-    list.files(pattern = ".eddypro", full.names = TRUE) %>%
-    read_eddypro_settings() %>% 
-    tibble::as_tibble() %>%
-    dplyr::mutate(dir = ep_dirs[i]) %>%
-    dplyr::relocate(dir)
-  
-  data_files[[i]] <- ep_paths[i] %>%
-    list.files(pattern = ".csv", full.names = TRUE, recursive = TRUE) %>% 
-    str_subset_eddypro()
-  files <- data_files[[i]]
-  files_col <- stringr::str_c(files, collapse = "|")
-  
-  # FULL OUTPUT FILE
-  if (stringr::str_detect(files_col, "full_output")) {
-    cat("full_output...")
-    file <- stringr::str_subset(files, "full_output")
-    temp <- read_eddypro(file, skip = 1)
-    purrr::pluck(lists, "full_output", i) <- temp
-    cat("done. ")
-  } 
-  
-  # METADATA FILE
-  if (stringr::str_detect(files_col, "metadata")) {
-    cat("metadata...")
-    file <- stringr::str_subset(files, "metadata")
-    temp <- read_eddypro(file, units = FALSE)
-    purrr::pluck(lists, "metadata", i) <- temp
-    cat("done. ")
-  }
-  
-  # BIOMET FILE
-  if (stringr::str_detect(files_col, "biomet")) {
-    cat("biomet...")
-    file <- stringr::str_subset(files, "biomet")
-    temp <- read_eddypro(file)
-    purrr::pluck(lists, "biomet", i) <- temp
-    cat("done. ")
-  }
-  
-  # FLUXNET FILE
-  if (stringr::str_detect(files_col, "fluxnet")) {
-    cat("fluxnet...")
-    file <- stringr::str_subset(files, "fluxnet")
-    temp <- read_eddypro(file, TIMESTAMP_END, units = FALSE)
-    purrr::pluck(lists, "fluxnet", i) <- temp
-    cat("done. ")
-  }
-  
-  # QC DETAILS FILE
-  if (stringr::str_detect(files_col, "qc_details")) {
-    cat("qc_details...")
-    file <- stringr::str_subset(files, "qc_details")
-    temp <- read_eddypro(file, skip = 1)
-    purrr::pluck(lists, "qc_details", i) <- temp
-    cat("done. ")
-  }
-  
-  # STATS FILE
-  if (stringr::str_detect(files_col, "st7")) {
-    cat("stats...")
-    file <- stringr::str_subset(files, "st7")
-    temp <- read_eddypro(file, skip = 1, units = FALSE)
-    purrr::pluck(lists, "st7", i) <- temp
+  for (j in seq_along(data_files[[i]])) {
+    cat(paste0(file_types[[i]][j], "..."))
+    temp <- quietly(read_eddypro)(
+      data_files[[i]][j], type = file_types[[i]][j], guess_max = 5000
+    )
+    pluck(data_ep, i, file_types[[i]][j]) <- temp$result
     cat("done. ")
   }
   
   cat("\n")
 }
 
-lists_sub <- lists %>%
-  purrr::map(rlang::set_names, basename(ep_paths)) %>%
-  purrr::map(purrr::discard, is.null)
+data_ep_t <- data_ep %>%
+  map(discard, is.null) %>%
+  transpose() %>%
+  map(discard, is.null)
 
 # Subset data based on correct calibrations if applicable
-recal <- control %>% 
-  purrr::pluck(settings$site, as.character(settings$year), "recal_periods") %>%
-  purrr::map(lubridate::as_datetime)
+recal <- map(md$recal_periods, as_datetime)
+# recal <- control %>% 
+#   pluck(settings$site, as.character(settings$year), "recal_periods") %>%
+#   map(as_datetime)
 
 if (!all(is.na(recal))) {
   
@@ -174,131 +127,75 @@ if (!all(is.na(recal))) {
   
   # Recal dir should be labeled with "_recal"
   # - only supports one recal dir (for now)
-  recal_dir <- stringr::str_subset(ep_dirs, "recal")
+  recal_dir <- str_subset(ep_dirs, "recal")
   
   # Get non-recal dirs
-  other_dirs <- stringr::str_subset(ep_dirs, "recal", negate = TRUE)
+  other_dirs <- str_subset(ep_dirs, "recal", negate = TRUE)
   
   # Build logical expressions for subsetting periods
   recal_logic <- 1:length(recal) %>% 
-    purrr::map(
-      ~ stringr::str_glue("between2(timestamp, recal[[", .x, "]])")
-    ) %>% 
+    map(~ str_glue("between2(timestamp, recal[[", .x, "]])")) %>% 
     glue::glue_collapse(sep = " | ") %>% 
-    stringr::str_glue("(", ., ")")
-  other_logic <- stringr::str_glue("!", recal_logic)
+    str_glue("(", ., ")")
+  other_logic <- str_glue("!", recal_logic)
   
   # Subset the data
-  lists_sub <- lists_sub %>%
+  data_ep_t <- data_ep_t %>%
     # Remove non-recal periods from recal data
-    purrr::map(
-      purrr::map_at, recal_dir, dplyr::filter, !!rlang::parse_expr(recal_logic)
-    ) %>%
+    map(map_at, recal_dir, filter, !!rlang::parse_expr(recal_logic)) %>%
     # Remove recal periods from non-recal data
-    purrr::map(
-      purrr::map_at, other_dirs, dplyr::filter, !!rlang::parse_expr(other_logic)
-    )
+    map(map_at, other_dirs, filter, !!rlang::parse_expr(other_logic))
+  
+  cat("done.\n")
 }
 
-cat("done.\n")
-cat("Coalescing data...")
+cat("Merging data...")
 
 # Combine and save the data as .csv files
-
-# Save original varnames and units
-varnames <- lists_sub %>%
-  purrr::map_depth(2, dplyr::select, -timestamp) %>%
-  purrr::map_depth(2, ~ openeddy::varnames(., names = TRUE)) %>%
-  purrr::map_depth(2, dplyr::bind_rows) %>%
-  purrr::map(dplyr::bind_rows) %>%
-  purrr::map(dplyr::slice, 1) %>%
-  purrr::map(unlist, use.names = TRUE) %>%
-  purrr::map(dplyr::na_if, "-") %>%
-  purrr::imap(~ dplyr::coalesce(.x, .y)) %>%
-  purrr::map(tidyr::replace_na, "-") %>%
-  purrr::map(unname)
-units <- lists_sub %>%
-  purrr::map_depth(2, dplyr::select, -timestamp) %>%
-  purrr::map_depth(2, ~ openeddy::units(., names = TRUE)) %>%
-  purrr::map_depth(2, dplyr::bind_rows) %>%
-  purrr::map(dplyr::bind_rows) %>%
-  purrr::map(dplyr::slice, 1) %>%
-  purrr::map(unlist, use.names = FALSE)
+data_merged <- data_ep_t %>% 
+  map(merge_eddypro) %>%
+  # Subset records by year
+  map(filter, year(timestamp - 900) %in% settings$year) 
 
 # Combine the data
-combined_lists <- lists_sub %>%
-  purrr::map(dplyr::bind_rows) %>%
-  purrr::map(dplyr::arrange, timestamp) %>%
-  # Subset year
-  purrr::map(
-    dplyr::filter, lubridate::year(timestamp - 900) %in% settings$year
-  ) 
+# combined_lists <- lists_sub %>%
+#   map(bind_rows) %>%
+#   map(arrange, timestamp) %>%
+#   # Subset year
+#   map(filter, year(timestamp - 900) %in% settings$year) 
 
 cat("done.\n")
-cat("Resolving duplicate timestamps: ")
-
-# Handle duplicated observations
-for (i in seq_along(combined_lists)) {
-  
-  name <- names(combined_lists)[i]
-  cat(name, "...", sep = "")
-  
-  # This check is expensive, so only do it if necessary
-  duplicates <- any(duplicated(combined_lists[[i]]$timestamp))
-  
-  if (duplicates) {
-    
-    combined_lists[[i]] <- combined_lists[[i]] %>% 
-      dplyr::rowwise() %>% 
-      # Number of NA values in each row
-      dplyr::mutate(n_na = sum(is.na(dplyr::c_across(is.numeric)))) %>% 
-      dplyr::group_by(timestamp) %>%
-      # Sort by least number of NAs per timestamp
-      dplyr::arrange(n_na, .by_group = TRUE) %>%
-      # Select the first (i.e. most complete) observation
-      dplyr::slice(1) %>%
-      dplyr::ungroup() %>%
-      dplyr::select(-n_na)
-  }
-  
-  cat("done. ")
-}
 
 # Add units and varnames back to data
-combined_lists <- combined_lists %>%
-  purrr::map(dplyr::select, -timestamp) %>%
-  purrr::map2(varnames, ~ add_attr(.x, "varnames", .y)) %>%
-  purrr::map2(units, ~ add_attr(.x, "units", .y))
+# combined_lists <- combined_lists %>%
+#   map(dplyr::select, -timestamp) %>%
+#   map2(varnames, ~ add_attr(.x, "varnames", .y)) %>%
+#   map2(units, ~ add_attr(.x, "units", .y))
 
 # Write combined data as single .csv files
 
-cat("\nWriting datasets: ")
-
 # Bind settings together and write as a table
 ep_settings_out <- file.path(
-  path_out, "settings", paste0("processing_combined_", tag_out, ".csv")
+  path_out, "settings", paste0("processing_combined_", tag, ".csv")
 )
-readr::write_csv(dplyr::bind_rows(ep_settings), ep_settings_out)
+write_csv(bind_rows(ep_settings), ep_settings_out)
 
-for (i in seq_along(combined_lists)) {
+files_out <- file.path(
+  path_out, 
+  names(data_merged), paste0(names(data_merged), "_combined_", tag, ".csv")
+)
+
+cat("\nWriting datasets: ")
+for (i in seq_along(data_merged)) {
   
-  name <- names(combined_lists)[i]
-  cat(name, "...", sep = "")
+  cat(names(data_merged)[i], "...", sep = "")
   
-  if (name %in% c("fluxnet", "st7")) {
-    # FLUXNET and stats outputs don't have units
-    readr::write_csv(
-      combined_lists[[i]],
-      file.path(path_out, name, paste0(name, "_combined_", tag_out, ".csv")),
-      na = "-9999"
-    )
-  } else {
-    openeddy::write_eddy(
-      combined_lists[[i]],
-      file.path(path_out, name, paste0(name, "_combined_", tag_out, ".csv")),
-      col.names = varnames[[i]]
-    )
+  # Each EddyPro output type gets its own folder
+  if (!dir.exists(dirname(files_out[i]))) {
+    dir.create(dirname(files_out[i]))
   }
+  
+  write_eddypro(data_merged[[i]], files_out[i])
   
   cat("done. ")
 }
@@ -309,61 +206,55 @@ for (i in seq_along(combined_lists)) {
 cat("\nPreparing FLUXNET data for further analysis...")
 
 # Prepare output dataset
-fluxnet <- combined_lists %>%
-  purrr::pluck("fluxnet") %>%
-  drop_all_attributes() %>%
-  dplyr::mutate(timestamp = lubridate::ymd_hm(TIMESTAMP_END)) %>%
-  dplyr::relocate(timestamp) %>%
+fluxnet <- data_merged %>%
+  pluck("fluxnet") %>%
+  drop_all_attrs() %>%
+  # mutate(timestamp = as_datetime(timestamp)) %>%
   # Convert all names to lowercase (better for R)
-  dplyr::rename_with(tolower) %>%
+  rename_with(tolower) %>%
   # Fix errors caused by duplicated column names
-  tibble::as_tibble(.name_repair = "universal") %>%
+  as_tibble(.name_repair = "unique") %>%
   # Remove columns that contain nothing (all NA)
-  dplyr::select_if(~ !all(is.na(.)))
+  select(-where(~ all(is.na(.x))))
 
 # Subset necessary data
-fluxnet <- fluxnet %>%
-  # Remove metadata and uncorrected data
-  dplyr::select(
-    -dplyr::starts_with("badm"), -dplyr::starts_with("inst"), 
-    -dplyr::starts_with("custom"), -dplyr::ends_with("method"), 
-    -dplyr::ends_with("uncorr"), -dplyr::matches("stage[[:digit:]]")
-  ) %>%
-  # Remove stats & QC details (this is ultimately pulled from full_output)
-  dplyr::select(
-    -dplyr::ends_with("kid"), -dplyr::ends_with("zcd"), 
-    -dplyr::ends_with("corrdiff"), -dplyr::ends_with("nsr"), 
-    -dplyr::ends_with("ss"), -dplyr::ends_with("itc"), 
-    -dplyr::ends_with("test"), -dplyr::ends_with("median"), 
-    -dplyr::ends_with("p25"), -dplyr::ends_with("p75"), 
-    -dplyr::ends_with("spikes"), -dplyr::ends_with("nrex"), 
-    -dplyr::ends_with("skw"), -dplyr::ends_with("kur")
-  )
+# fluxnet <- fluxnet %>%
+#   # Remove metadata and uncorrected data
+#   select(
+#     -starts_with("badm"), -starts_with("inst"), -starts_with("custom"), 
+#     -ends_with("method"), -ends_with("uncorr"), -matches("stage[[:digit:]]")
+#   ) %>%
+#   # Remove stats & QC details (this is ultimately pulled from full_output)
+#   select(
+#     -ends_with("kid"), -ends_with("zcd"), -ends_with("corrdiff"), 
+#     -ends_with("nsr"), -ends_with("ss"), -ends_with("itc"), 
+#     -ends_with("test"), -ends_with("median"), -ends_with("p25"), 
+#     -ends_with("p75"), -ends_with("spikes"), -ends_with("nrex"), 
+#     -ends_with("skw"), -ends_with("kur")
+#   )
 
 # Make sure timestamp forms a regular sequence for the entire year
-fluxnet <- tibble::tibble(
+fluxnet <- tibble(
   timestamp = create_timesteps(settings$year, 48, shift_by = 30)
 ) %>%
-  dplyr::left_join(fluxnet, by = "timestamp")
+  left_join(fluxnet, by = "timestamp")
 
 cat("done.\n")
 cat("Writing FLUXNET data...")
 
 # Save the combined fluxnet files as .csv file
 fn_out <- file.path(
-  path_out, "eddypro", paste0("eddypro_combined_", tag_out, ".csv")
+  path_out, "eddypro", paste0("eddypro_combined_", tag, ".csv")
 )
-readr::write_csv(fluxnet, fn_out)
+write_csv(fluxnet, fn_out)
 
 # Create documentation
 fn_docu <- settings
 fn_docu$files <- ep_paths %>% 
-  purrr::map(
-    list.files, pattern = ".csv", full.names = TRUE, recursive = TRUE
-  ) %>% 
-  purrr::simplify() %>% 
-  stringr::str_subset("fluxnet")
-fn_docu_out <- stringr::str_replace(fn_out, ".csv", ".txt")
+  map(list.files, pattern = ".csv", full.names = TRUE, recursive = TRUE) %>% 
+  simplify() %>% 
+  str_subset("fluxnet")
+fn_docu_out <- str_replace(fn_out, ".csv", ".txt")
 # Save documentation
 sink(fn_docu_out)
 fn_docu
--- processing/02_correct_eddypro.R
+++ processing/02_correct_eddypro.R
@@ -16,18 +16,12 @@
 start_time <- Sys.time()
 
 # Load the required packages
+devtools::load_all("~/Projects/flux/dscalr")
 library(lubridate)
 library(tidyverse)
 
 # Load reference files
-source("~/Desktop/DATA/Flux/tools/reference/site_metadata.R")
-
-# Load functions
-source("~/Desktop/DATA/Flux/tools/engine/functions/clean.R")
-source("~/Desktop/DATA/Flux/tools/engine/functions/dates_and_times.R")
-source("~/Desktop/DATA/Flux/tools/engine/functions/flag.R")
-source("~/Desktop/DATA/Flux/tools/engine/functions/latest_version.R")
-source("~/Desktop/DATA/Flux/tools/engine/functions/utilities.R")
+# source("reference/site_metadata.R")
 
 
 ### Helper functions ===========================================================
@@ -39,15 +33,19 @@ potential_radiation <- function(timestamp, md) {
   yday <- lubridate::yday(timestamp)
   hour <- decimal_hour(timestamp)
   
-  bigleaf::potential.radiation(yday, hour, md$lat, md$lon, md$tz)
+  bigleaf::potential.radiation(
+    yday, hour, md$tower$coords$lat, md$tower$coords$lon, md$tz$utc_offset
+  )
 }
 
 sun_position <- function(timestamp, md) {
   
   # solartime function gets tz from timestamp, so need to encode attribute
-  timestamp <- lubridate::force_tz(timestamp, md$tz_name)
+  timestamp <- lubridate::force_tz(timestamp, md$tz$name)
   
-  sun_pos <- solartime::computeSunPosition(timestamp, md$lat, md$lon)
+  sun_pos <- solartime::computeSunPosition(
+    timestamp, md$tower$coords$lat, md$tower$coords$lon
+  )
   
   sun_pos %>% 
     tibble::as_tibble() %>% 
@@ -106,27 +104,34 @@ flag_biomet_system <- function(data, vars = c(ta_bm, lw_in, lw_out)) {
 ### Initialize script settings & documentation =================================
 
 # Load metadata file
-md <- purrr::pluck(site_metadata, settings$site)
+md <- yaml::read_yaml(file.path("data", settings$site, "metadata.yml"))
+
+# Set tag for creating output file names
+tag <- make_tag(settings$site, settings$year)
 
 # Set the desired working directory in RStudio interface
 # - assumes that the subdirectory structure is already present
-wd <- file.path("~/Desktop", "DATA", "Flux", settings$site, settings$year)
-path_in <- file.path(wd, "processing", "01_combine_eddypro")
+wd <- file.path("data", settings$site, settings$year)
+path_in <- file.path(wd, "01_combine_eddypro")
 
 # Input files
-data_input <- latest_version(file.path(path_in, "eddypro"))
-ep_settings_input <- latest_version(file.path(path_in, "settings"))
-
-# Set tag for creating output file names
-tag_out <- create_tag(settings$site, settings$year, settings$date)
+file_data <- file.path(
+  wd, "01_combine_eddypro", "eddypro", paste0("eddypro_combined_", tag, ".csv")
+)
+# file_data <- latest_version(file.path(path_in, "eddypro"))
+file_settings <- file.path(
+  wd, "01_combine_eddypro", "settings", 
+  paste0("processing_combined_", tag, ".csv")
+)
+# file_settings <- latest_version(file.path(path_in, "settings"))
 
 # Set path for output files
-path_out <- file.path(wd, "processing", "02_correct_eddypro")
+path_out <- file.path(wd, "02_correct_eddypro")
 
 # List of vars housed in the biomet system
 biomet_vars <- rlang::exprs(
   ta_bm, rh_bm, ppfd_in, sw_in, sw_out, lw_in, lw_out, p_rain,
-  dplyr::matches(c("^ts_|^swc_|^g_"))
+  matches(c("^ts_|^swc_|^g_"))
 )
 
 
@@ -135,19 +140,18 @@ biomet_vars <- rlang::exprs(
 cat("Importing data files...")
 
 # Read in data
-data <- readr::read_csv(
-  data_input, guess_max = 6000, 
-  col_types = readr::cols(.default = readr::col_guess()), progress = FALSE
+data <- read_csv(
+  file_data, guess_max = 7000, col_types = cols(), progress = FALSE
 )
 
 # Fix names
 data <- data %>%
   # Simplify non-replicated variable names (remove "_1_1_1")
-  dplyr::rename_with(
-    ~ stringr::str_replace(., "_\\d_1_1", ""),
-    c(dplyr::matches("_\\d_1_1"), -dplyr::matches(c("^ts_|^swc_|^g_"))),
+  rename_with(
+    ~ str_replace(., "_\\d_1_1", ""),
+    c(matches("_\\d_1_1"), -matches(c("^ts_|^swc_|^g_"))),
   ) %>%
-  dplyr::rename(
+  rename(
     # Only one air pressure var, so remove suffix
     pa = pa_ep,
     # Give biomet ta & rh vars a suffix 
@@ -157,8 +161,8 @@ data <- data %>%
 
 # Flag/clean system errors indicated by simultaneous runs in biomet vars
 data <- data %>% 
-  dplyr::mutate(qc_biomet_all = flag_biomet_system(.)) %>%
-  dplyr::mutate(dplyr::across(c(!!!biomet_vars), ~ clean(.x, qc_biomet_all)))
+  mutate(qc_biomet_all = flag_biomet_system(.)) %>%
+  mutate(across(c(!!!biomet_vars), ~ clean(.x, qc_biomet_all)))
 
 cat("done.\n")
 
@@ -172,22 +176,19 @@ cat("Applying names & units corrections...")
 
 # Check p_rain before converting
 if (max(data$p_rain, na.rm = TRUE) < 1) {
-  data <- dplyr::mutate(data, p_rain = p_rain * 1000)
+  data <- mutate(data, p_rain = p_rain * 1000)
 }
 
 # Automatic correction of temperatures and percentages
 data <- data %>%
-  dplyr::mutate(dplyr::across(
-    dplyr::starts_with("swc_"), ~ dplyr::if_else(.x > 1, .x / 100, .x)
-  )) %>%
-  dplyr::mutate(dplyr::across(
-    c(dplyr::starts_with("ts_"), ta_ep, ta_bm), 
-    ~ dplyr::if_else(.x > 150, .x - 273.15, .x)
+  mutate(across(starts_with("swc_"), ~ if_else(.x > 1, .x / 100, .x))) %>%
+  mutate(across(
+    c(starts_with("ts_"), ta_ep, ta_bm), ~ if_else(.x > 150, .x - 273.15, .x)
   ))
 
 # CH4 variables from nmol to umol
 if (median(data$fch4, na.rm = TRUE) > 1) {
-  data <- dplyr::mutate(data, dplyr::across(
+  data <- mutate(data, across(
     c(
       fch4, fch4_randunc_hf, sch4_single, ch4_molar_density, ch4_mixing_ratio, 
       ch4, ch4_meas_sigma
@@ -203,20 +204,21 @@ cat("done.\n")
 mag_decl <- NA
 
 # Read in EddyPro settings
-ep_settings <- readr::read_csv(
-  ep_settings_input, col_types = readr::cols(.default = readr::col_guess())
-)
+ep_settings <- read_csv(file_settings, col_types = cols())
 
 # Check if geographic north was already used before correcting
 if (sum(ep_settings$use_geo_north) == 0) {
   
   # Calculate magnetic declination given location & time
-  mag_decl <- purrr::pluck(
-    oce::magneticField(md$lon, md$lat, settings$year + 0.5), "declination"
+  mag_decl <- pluck(
+    oce::magneticField(
+      md$tower$coords$lon, md$tower$coords$lat, settings$year + 0.5
+    ), 
+    "declination"
   )
   
   # Reassign magnetic wind direction, apply declination
-  data <- dplyr::mutate(
+  data <- mutate(
     data,
     wd_mag = wd,
     wd = (wd_mag - mag_decl) %% 360
@@ -229,13 +231,13 @@ if (sum(ep_settings$use_geo_north) == 0) {
 cat("Combining replicated variables...")
 
 data <- data %>%
-  dplyr::rowwise() %>%
-  dplyr::mutate(
+  rowwise() %>%
+  mutate(
     g = mean(c(g_1_1_1, g_2_1_1, g_3_1_1)),
     swc = mean(c(swc_1_1_1, swc_2_1_1, swc_3_1_1)),
     ts = mean(c(ts_1_1_1, ts_2_1_1, ts_3_1_1))
   ) %>%
-  dplyr::ungroup()
+  ungroup()
 
 cat("done.\n")
 
@@ -247,19 +249,19 @@ cat("Correcting radiation for zero-offset...")
 # Radiation offset
 offset <- data %>%
   # Calculate sw_in_pot and sol_ang at end of averaging period
-  dplyr::mutate(
+  mutate(
     sw_in_pot = potential_radiation(timestamp, md),
     sol_ang = sun_position(timestamp, md),
     # Stricter sun time to exclude any possible period with light
     sun_time = sun_time(sol_ang, shoulder_n = 3)
   ) %>%
-  dplyr::filter(sw_in_pot == 0, sun_time == "night") %>%
-  dplyr::summarize(dplyr::across(
+  filter(sw_in_pot == 0, sun_time == "night") %>%
+  summarize(across(
     c(ppfd_in, sw_in, sw_out), mean, na.rm = TRUE)
   ) %>%
   as.list()
 
-data <- dplyr::mutate(
+data <- mutate(
   data, 
   ppfd_in = apply_offset(ppfd_in, -offset$ppfd_in),
   sw_in = apply_offset(sw_in, -offset$sw_in),
@@ -277,12 +279,12 @@ cat("Calculating additional variables...")
 
 # Get coefficient of ppfd_in/sw_in relationship
 frac_ppfd <- data %>% 
-  dplyr::select(sw_in, ppfd_in) %>% 
-  tidyr::drop_na() %>% 
-  dplyr::summarize(sum(sw_in) / sum(ppfd_in)) %>% 
-  purrr::pluck(1)
+  select(sw_in, ppfd_in) %>% 
+  drop_na() %>% 
+  summarize(sum(sw_in) / sum(ppfd_in)) %>% 
+  pluck(1)
 
-data <- dplyr::mutate(
+data <- mutate(
   data,
   # Calculate sw_in equivalent of ppfd_in
   sw_in_ppfd = ppfd_in * frac_ppfd,
@@ -307,18 +309,18 @@ cat("Writing output...")
 
 # Save the corrected data as a .csv file
 data_out <- file.path(
-  path_out, "data", paste0("eddypro_corrected_", tag_out, ".csv")
+  path_out, "data", paste0("eddypro_corrected_", tag, ".csv")
 )
-readr::write_csv(data, data_out)
+write_csv(data, data_out)
 
 # Create documentation for data output
-data_docu <- purrr::prepend(
+data_docu <- prepend(
   settings, list(
-    files = data_input, mag_decl = mag_decl, frac_ppfd = frac_ppfd, 
+    files = file_data, mag_decl = mag_decl, frac_ppfd = frac_ppfd, 
     offsets = offset
   ), before = length(settings)
 )
-data_docu_out <- stringr::str_replace(data_out, ".csv", ".txt")
+data_docu_out <- str_replace(data_out, ".csv", ".txt")
 
 # Save documentation
 sink(data_docu_out)
